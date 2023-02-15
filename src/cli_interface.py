d519e19b72ea948623912c56c161bdc6779a65c2
Jesper <jru@cvation.com>
2023-02-15 11:06:28 +0100

    refactor

--- src/cli_interface.py
+++ src/cli_interface.py
@@ -5,34 +5,15 @@ import requests
 import jsonschema
 
 from src.core.bt_graph import BTGraph
-
-
 from src.plantuml.plantuml_file_creator import (
     plantuml_diagram_creator_entire_domain,
     plantuml_diagram_creator_sub_domains,
 )
 
-
-def read_config_file(config_path):
-    schema_url = "https://raw.githubusercontent.com/Perlten/Master-thesis-rename/feature/json-config/config.schema.json"
-    config = None
-    with open(config_path, "r") as f:
-        config = json.load(f)
-
-    schema = requests.get(schema_url).json()
-
-    jsonschema.validate(instance=config, schema=schema)
-
-    config["_config_path"] = os.path.dirname(config_path)
-    return config
-
-
 def render(config_path: str):
     config = read_config_file(config_path)
     g = BTGraph()
     g.build_graph(config)
-
-    ignore_modules = ["test", "tool", "util", "exercise", ]
 
     project_name = config.get("name")
 
@@ -55,6 +36,18 @@ def render(config_path: str):
             save_location=config.get("saveLocation"),
         )
 
+def read_config_file(config_path):
+    schema_url = "https://raw.githubusercontent.com/Perlten/Master-thesis-rename/feature/json-config/config.schema.json"
+    config = None
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    schema = requests.get(schema_url).json()
+
+    jsonschema.validate(instance=config, schema=schema)
+
+    config["_config_path"] = os.path.dirname(config_path)
+    return config
 
 def main():
     typer.run(render)
