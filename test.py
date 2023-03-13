1e16d46be3bf441bbacd6ecb84163ed6f50caf2f
Jesper <jru@cvation.com>
2023-03-13 21:53:58 +0100

    windows-fix

--- test.py
+++
@@ -1,9 +0,0 @@
-my_string = "This is a string with \\double backslashes"
-
-if "\\" in my_string:
-
-    split_string = my_string.split("\\")
-
-    split_string = "/".join(split_string)
-
-    print(split_string)
