ee4fda16c35f1b14a30bbbb4b6ce3b3ecb3934e5
Nikolai Perlt <nikolaiperlt@outlook.com>
2023-04-06 12:00:22 +0200

    schema stuff

--- src/plantumlv2/pu_manager.py
+++ src/plantumlv2/pu_manager.py
@@ -205,7 +205,7 @@ def _filter_packages(
             if isinstance(package_view, dict):
                 filter_path = package_view["packagePath"].replace(".", "/")
                 view_depth = package_view["depth"]
-                if package.path == filter_path:
+                if package.path == filter_path or filter_path == "":
                     filtered_packages_set.add(package)
                     depth_filter_packages = _find_packages_with_depth(
                         package, view_depth, packages_map
